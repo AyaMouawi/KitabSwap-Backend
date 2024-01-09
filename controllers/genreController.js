ac0657921e11440af44c5252f74c7cd1473c1fe0
AyaMouawi <ayamouawi8@gmail.com>
2024-01-09 14:49:57 +0200

    bookCount in getAll genres

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,22 +1,27 @@
 const connection = require('../config/db');
 
 const getAll = async (_, res) => {
-    const query = `SELECT * FROM genres`;
-    try {
-      const [response] = await connection.query(query);
-      return res.status(200).json({
-        success: true,
-        message: `All genres retrieved successfully.`,
-        data: response,
-      });
-    } catch (error) {
-      return res.status(400).json({
-        success: false,
-        message: `Unable to retrieve all genres.`,
-        error: error.message,
-      });
-    }
-  };
+  const query = `
+    SELECT genres.*, COUNT(salebooks.saleBook_id) AS bookCount
+    FROM genres
+    LEFT JOIN salebooks ON genres.genre_id = salebooks.genre_id
+    GROUP BY genres.genre_id
+  `;
+  try {
+    const [response] = await connection.query(query);
+    return res.status(200).json({
+      success: true,
+      message: `All genres retrieved successfully.`,
+      data: response,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: `Unable to retrieve all genres.`,
+      error: error.message,
+    });
+  }
+};
 
 
   const getById = async (req, res) => {
